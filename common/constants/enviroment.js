10dbca6186671b033cf16f161e3efab53f7658d2
Rahul Sharma <yantralogistics@gmail.com>
2021-08-17 13:22:58 +0530

    packstotrakkia11

--- common/constants/enviroment.js
+++ common/constants/enviroment.js
@@ -1,3 +1,3 @@
-export const DEFAULT_BASE_URL = 'https://tbackend.yantraksh.com/';
+export const DEFAULT_BASE_URL = 'https://tbackend.trakkia.com/';
 // export const DEFAULT_BASE_URL = 'http://127.0.0.1:8000';
 
