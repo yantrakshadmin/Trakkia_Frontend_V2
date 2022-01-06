9ab7456b00ce1b05d363876afddc397ba883fca1
Mudit Jain <muditj148@gmail.com>
2022-01-06 19:56:16 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.574db9f8d4137b98c02bdcef5d010684.js"
+  "/precache-manifest.5d1186fa3a944fc2fda0dc0921c590e8.js"
 );
 
 self.addEventListener('message', (event) => {
--- static/js/91.4b5ae93a.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpweb=this.webpackJsonpweb||[]).push([[91],{855:function(e,t,n){"use strict";n.r(t);var a=n(0),r=n.n(a),s=n(43);t.default=function(){return r.a.createElement(s.b,{to:"/client/material-request/"})}}}]);
-//# sourceMappingURL=91.4b5ae93a.chunk.js.map
